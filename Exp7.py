727691f16c6ad9da861b576db50b7a2e1f10db82
Suhani Koul <106395135+suhani138@users.noreply.github.com>
2023-11-20 23:58:57 +0530

    Create Exp7.py

---
+++ Exp7.py
@@ -0,0 +1,26 @@
+# create an empty list to store the database
+database = []
+
+# function to add a new record to the database
+def add_record(name, age, city):
+    record = {'Name': name, 'Age': age, 'City': city}
+    database.append(record)
+    print('Record added successfully')
+
+# function to display all the records in the database
+def display_records():
+    for record in database:
+        print(record)
+
+# main program
+while True:
+    print('Enter your name, age, and city:')
+    name = input('Name: ')
+    age = int(input('Age: '))
+    city = input('City: ')
+    add_record(name, age, city)
+    print('Do you want to add another record? (y/n)')
+    choice = input()
+    if choice == 'n':
+        break
+display_records()
